89fa898c74e72e4b01aa3810308d721eb5ac5ccf
zhoutianjiang <892403353@qq.com>
2018-09-07 20:07:19 +0800

    python

--- TSP/TNX2.py
+++ TSP/TNX2.py
@@ -11,18 +11,34 @@ step0 = 200
 n = 10000
 k = 20
 p = 0.05
-DD = []
-'''D = [[0, 1, 52, 73, 15, 45, 37, 34, 70, 1],
-     [1, 0, 1, 27, 32, 96, 39, 33, 43, 74],
-     [52, 1, 0, 1, 36, 52, 52, 69, 13, 63],
-     [73, 27, 1, 0, 1, 79, 48, 88, 8, 14],
-     [15, 32, 36, 1, 0, 1, 69, 6, 94, 67],
-     [45, 96, 52, 79, 1, 0, 1, 97, 22, 86],
-     [37, 39, 52, 48, 69, 1, 0, 1, 87, 69],
-     [34, 33, 69, 88, 6, 97, 1, 0, 1, 22],
-     [70, 43, 13, 8, 94, 22, 87, 1, 0, 1],
-     [1, 74, 63, 14, 67, 86, 69, 22, 1, 0]]'''
-'''D = [[0, 1, 52, 73, 15, 45, 37, 1],
+D = [[0, 1, 10, 20, 30, 40, 50, 34, 70, 10, 52, 73, 15, 45, 37, 34, 70, 10, 52, 1],
+     [1, 0, 1, 27, 32, 96, 39, 33, 43, 74, 27, 32, 96, 39, 33, 43, 74, 27, 32, 96],
+     [52, 1, 0, 1, 36, 52, 52, 69, 13, 63, 36, 52, 52, 69, 13, 63, 36, 52, 52, 69],
+     [73, 27, 1, 0, 1, 79, 48, 88, 8, 14, 48, 88, 8, 14, 48, 88, 8, 14, 48, 88, 8],
+     [15, 32, 36, 1, 0, 1, 69, 6, 94, 67, 69, 6, 94, 67, 69, 6, 94, 67, 69, 6, 94],
+     [45, 96, 52, 79, 1, 0, 1, 97, 22, 86, 69, 6, 94, 67, 69, 6, 94, 67, 69, 6, 94],
+     [37, 39, 52, 48, 69, 1, 0, 1, 87, 69, 69, 6, 94, 67, 69, 6, 94, 67, 69, 6, 94],
+     [34, 33, 69, 88, 6, 97, 1, 0, 1, 22, 69, 6, 94, 67, 69, 6, 94, 67, 69, 6, 94],
+     [70, 43, 13, 8, 94, 22, 87, 1, 0, 1, 69, 6, 94, 67, 69, 6, 94, 67, 69, 6, 94],
+     [1, 74, 63, 14, 67, 86, 69, 22, 1, 0, 1, 69, 6, 94, 67,69, 6, 94, 6769, 6, 94],
+     [1, 74, 63, 14, 67, 86, 69, 22, 1, 10, 0, 1, 6, 94, 67,69, 6, 94, 6769, 6, 94],
+     [1, 74, 63, 14, 67, 86, 69, 22, 1, 0, 10, 0, 1, 94, 67,69, 6, 94, 6769, 6, 94],
+     [1, 74, 63, 14, 67, 86, 69, 22, 1, 0, 10, 69, 0, 1, 67,69, 6, 94, 6769, 6, 94],
+     [1, 74, 63, 14, 67, 86, 69, 22, 1, 0, 10, 69, 6, 0, 1,69, 6, 94, 6769, 6, 94],
+     [1, 74, 63, 14, 67, 86, 69, 22, 1, 0, 10, 69, 6, 94, 0, 1, 6, 94, 6769, 6, 94],
+     [1, 74, 63, 14, 67, 86, 69, 22, 1, 0, 10, 69, 6, 94, 67, 0, 1, 94, 6769, 6, 94],
+     [1, 74, 63, 14, 67, 86, 69, 22, 1, 0, 10, 69, 0, 1, 67,69, 0, 1, 6769, 6, 94],
+     [1, 74, 63, 14, 67, 86, 69, 22, 1, 0, 10, 69, 6, 0, 1,69, 6, 0, 1, 6, 94],
+     [1, 74, 63, 14, 67, 86, 69, 22, 1, 0, 10, 69, 6, 94, 0, 1, 6, 0, 1, 6, 94],
+     [1, 74, 63, 14, 67, 86, 69, 22, 1, 0, 10, 69, 6, 94, 67, 0, 1,10 , 0, 1, 94],
+     [1, 74, 63, 14, 67, 86, 69, 22, 1, 0, 10, 69, 6, 94, 0, 1, 6, 0, 1, 6, 0]
+     ]
+for i in range(20):
+    for j in range(20):
+        D[i][j]=D[j][i]
+        D[i][i]=0
+'''
+D = [[0, 1, 52, 73, 15, 45, 37, 1],
      [1, 0, 1, 27, 32, 96, 39, 33],
      [52, 1, 0, 1, 36, 52, 52, 69],
      [73, 27, 1, 0, 1, 79, 48, 88],
@@ -36,7 +52,7 @@ Count = 0
 
 
 def fit(x, y):
-    return DD[x][y]
+    return D[x][y]
 
 
 def f(x):
@@ -45,35 +61,6 @@ def f(x):
         s += fit(x[i], x[i+1])
     s += fit(x[k-1], x[0])
     return s
-
-
-'''def mod(x):
-    for i in range(k):
-        x[i] = int(round(x[i]))
-    for i in range(k):
-        if x[i]>k-1:
-            x[i]=0
-        if x[i]<0:
-            x[i]=k-1
-    lx = [i for i in range(k)]
-    lx = random.sample(lx, k)
-    ll=[]
-    flag=[]
-    t=0
-    for i in x:
-        if i not in ll:
-            ll.append(i)
-            lx.remove(i)
-        else:
-            flag.append(t)
-        t+=1
-    for i in flag:
-        a = random.choice(lx)
-        ll.insert(i, a)
-        lx.remove(a)
-    return ll'''
-
-
 
 
 def mod(x):
@@ -85,6 +72,7 @@ def mod(x):
         if x[i]<0:
             x[i]=k-1
     lx = [i for i in range(k)]
+    lx = random.sample(lx, k)
     ll=[]
     flag=[]
     t=0
@@ -107,55 +95,6 @@ def mod(x):
     return ll
 
 
-'''def mod(x):
-    for i in range(k):
-        x[i] = int(round(x[i]))
-    for i in range(k):
-        if x[i]>k-1:
-            x[i]=0
-        if x[i]<0:
-            x[i]=k-1
-    lx = [i for i in range(k)]
-    lx = random.sample(lx, k)
-    ll=[]
-    for i in x:
-        if i not in ll:
-            ll.append(i)
-            a=i
-        else:
-            a = random.choice(lx)
-            ll.append(a)
-        lx.remove(a)
-    return ll'''
-
-
-def readData(name):
-    with open(name, 'r') as f:
-        f.seek(128)
-        lf = f.readlines()
-        ll=[]
-        for line in lf:
-            ll.append(line.strip().split(' '))
-        data = []
-        for i in ll:
-            d = []
-            for j in i:
-                if j == 'EOF' or j=='':
-                    break
-                d.append(float(j))
-            data.append(d)
-        print(data)
-        YY=[]
-
-        for i in range(52):
-            YLine = []
-            for j in range(52):
-                xd = data[i][1]-data[j][1]
-                yd = data[i][2]-data[j][2]
-                YLine.append(int(np.sqrt(xd**2+yd**2)))
-            YY.append(YLine)
-        return YY
-
 def run():
     global step
     global Count
@@ -170,7 +109,7 @@ def run():
     best.append(fbest)
 
     for i in range(n):
-        rand = random.random()
+        rand =random.random()
         if rand<p:
             step = step0
         d = step / c
@@ -203,7 +142,6 @@ def run():
 
 
 if __name__ == '__main__':
-    DD = readData('berlin52.tsp')
     run()
     print(Count)
     x = [i for i in range(n+1)]
@@ -218,12 +156,3 @@ if __name__ == '__main__':
     plt.xticks(xi)
     plt.title("TNX SOLVE TSP")
     plt.show()
-    '''x=0
-    for  i in range(50):
-        run()
-        print(Count)
-        x+=Count
-    print(x/50)'''
-
-
-
